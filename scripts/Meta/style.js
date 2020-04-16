d4152df3ac66f4fccdcac00201b73e6c723f9f81
David Zulaica <zulaica@gmail.com>
2020-04-16 02:31:29 -0700

    Delete unused module

--- scripts/Meta/style.js
+++ scripts/Meta/style.js
@@ -1,5 +1,4 @@
 import { backgroundStyle } from './background.js';
-import { titleStyle } from './title.js';
 import { figureStyle } from './figure.js';
 import { imgContainerStyle } from './imgContainer.js';
 import { imgStyle } from './img.js';
@@ -8,7 +7,6 @@ import { figcaptionStyle } from './figcaption.js';
 const style = document.createElement('style');
 style.textContent = `
   ${backgroundStyle}
-  ${titleStyle}
   ${figureStyle}
   ${imgContainerStyle}
   ${imgStyle}
