7db3445d22797dece2adc93ca65b0e64fd45b83b
David Zulaica <zulaica@gmail.com>
2020-04-21 03:24:01 -0700

    Remove redundant initial value

--- scripts/Meta/img.js
+++ scripts/Meta/img.js
@@ -3,7 +3,6 @@ const img = document.createElement('img');
 export const imgStyle = `
   img {
     width: 100%;
-    height: auto;
     -webkit-clip-path: polygon(
       5% 0, 0 5%, 0 95%, 5% 100%, 95% 100%, 100% 95%, 100% 5%, 95% 0
     );
