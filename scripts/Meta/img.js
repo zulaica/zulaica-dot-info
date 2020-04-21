63d4b5eec9a90c9ac56f3abb821e7a09cd99822b
David Zulaica <zulaica@gmail.com>
2020-04-21 02:32:51 -0700

    Calculate image height automatically

--- scripts/Meta/img.js
+++ scripts/Meta/img.js
@@ -3,7 +3,7 @@ const img = document.createElement('img');
 export const imgStyle = `
   img {
     width: 33vw;
-    height: 33vw;
+    height: auto;
     -webkit-clip-path: polygon(
       5% 0, 0 5%, 0 95%, 5% 100%, 95% 100%, 100% 95%, 100% 5%, 95% 0
     );
