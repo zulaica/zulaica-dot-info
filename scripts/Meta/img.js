d6e2a83e269dd2b20fea36d6352a549cbad0065d
David Zulaica <zulaica@gmail.com>
2020-06-02 11:54:26 -0700

    Center alt text

--- scripts/Meta/img.js
+++ scripts/Meta/img.js
@@ -3,6 +3,7 @@ const img = document.createElement("img");
 export const imgStyle = `
   img {
     width: 100%;
+    text-align: center;
     -webkit-clip-path: polygon(
       5% 0, 0 5%, 0 95%, 5% 100%, 95% 100%, 100% 95%, 100% 5%, 95% 0
     );
