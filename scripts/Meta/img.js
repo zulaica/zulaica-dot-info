9a5ac36db271b132ee9bdf993ff9897abbb8ee50
David Zulaica <zulaica@gmail.com>
2020-06-15 01:22:47 -0700

    Remove clip-path from image

--- scripts/Meta/img.js
+++ scripts/Meta/img.js
@@ -4,12 +4,6 @@ export const imgStyle = `
   img {
     width: 100%;
     text-align: center;
-    -webkit-clip-path: polygon(
-      5% 0, 0 5%, 0 95%, 5% 100%, 95% 100%, 100% 95%, 100% 5%, 95% 0
-    );
-    clip-path: polygon(
-      5% 0, 0 5%, 0 95%, 5% 100%, 95% 100%, 100% 95%, 100% 5%, 95% 0
-    );
   }
 `;
 
