7689f51739f9a9ecd251b4d84274c43d0e6c3287
David Zulaica <zulaica@gmail.com>
2020-06-15 01:43:09 -0700

    Set clip-path value as a variable

--- scripts/Meta/imgContainer.js
+++ scripts/Meta/imgContainer.js
@@ -21,12 +21,8 @@ export const imgContainerStyle = `
     height: 33vw;
     border-radius: 2px;
     background: #fff;
-    -webkit-clip-path: polygon(
-      0px 0px, 0px 100%, 100% 100%, 100% 0px, 0px 0px, 2.5% 2.5%, 5% 1px, 95% 1px, calc(33vw - 1px) 5%, calc(33vw - 1px) 95%, 95% calc(33vw - 1px), 5% calc(33vw - 1px), 1px 95%, 1px 5%, 2.5% 2.5%
-    );
-    clip-path: polygon(
-      0px 0px, 0px 100%, 100% 100%, 100% 0px, 0px 0px, 2.5% 2.5%, 5% 1px, 95% 1px, calc(33vw - 1px) 5%, calc(33vw - 1px) 95%, 95% calc(33vw - 1px), 5% calc(33vw - 1px), 1px 95%, 1px 5%, 2.5% 2.5%
-    );
+    -webkit-clip-path: polygon(var(--clip-path));
+    clip-path: polygon(var(--clip-path));
   }
 `;
 
