40dca051cb2b850784e6707887ecc23d2023241d
David Zulaica <zulaica@gmail.com>
2020-06-15 01:36:49 -0700

    Applly photo corner effect with pseudo element overlay

--- scripts/Meta/img.js
+++ scripts/Meta/img.js
@@ -4,6 +4,7 @@ export const imgStyle = `
   img {
     width: 100%;
     text-align: center;
+    border-radius: 2px;
   }
 `;
 
--- scripts/Meta/imgContainer.js
+++ scripts/Meta/imgContainer.js
@@ -13,6 +13,21 @@ export const imgContainerStyle = `
     border-radius: 3px;
     box-shadow: var(--shade);
   }
+
+  div::after {
+    content: "";
+    position: absolute;
+    width: 33vw;
+    height: 33vw;
+    border-radius: 2px;
+    background: #fff;
+    -webkit-clip-path: polygon(
+      0px 0px, 0px 100%, 100% 100%, 100% 0px, 0px 0px, 2.5% 2.5%, 5% 1px, 95% 1px, calc(33vw - 1px) 5%, calc(33vw - 1px) 95%, 95% calc(33vw - 1px), 5% calc(33vw - 1px), 1px 95%, 1px 5%, 2.5% 2.5%
+    );
+    clip-path: polygon(
+      0px 0px, 0px 100%, 100% 100%, 100% 0px, 0px 0px, 2.5% 2.5%, 5% 1px, 95% 1px, calc(33vw - 1px) 5%, calc(33vw - 1px) 95%, 95% calc(33vw - 1px), 5% calc(33vw - 1px), 1px 95%, 1px 5%, 2.5% 2.5%
+    );
+  }
 `;
 
 export default imgContainer;
