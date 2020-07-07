bfb28658e36ff817724db75baaa6230e59478357
David Zulaica <zulaica@gmail.com>
2020-07-07 05:35:57 -0700

    Explicitly set image dimensions for Safari

--- scripts/Meta/img.js
+++ scripts/Meta/img.js
@@ -3,6 +3,8 @@ const img = document.createElement("img");
 export const imgStyle = `
   img {
     object-fit: contain;
+    width: 100%;
+    height: 100%;
     text-align: center;
     border-radius: var(--border-radius);
   }
