6746f7e8974dae3da2d4bdf5b2d64c0b9c7e577c
David Zulaica <zulaica@gmail.com>
2020-07-13 18:16:24 -0700

    Remove properties made redunant by `object-fit`

--- scripts/Meta/img.js
+++ scripts/Meta/img.js
@@ -3,8 +3,6 @@ const img = document.createElement("img");
 export const imgStyle = `
   img {
     object-fit: contain;
-    width: 100%;
-    height: 100%;
     text-align: center;
     border-radius: var(--border-radius);
   }
