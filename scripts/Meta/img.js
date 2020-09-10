f20ec07ee222e360a14dcca9f7b1a73e583a0e46
David Zulaica <zulaica@gmail.com>
2020-09-09 22:39:15 -0700

    Prevent overflow with pano images

--- scripts/Meta/img.js
+++ scripts/Meta/img.js
@@ -3,6 +3,7 @@ const img = document.createElement("img");
 export const imgStyle = `
   img {
     object-fit: contain;
+    width: 100%;
     text-align: center;
     border-radius: var(--border-radius);
   }
