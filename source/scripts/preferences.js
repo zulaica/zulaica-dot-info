69102606855cb6744004351ee16207cf7a3f5325
David Zulaica <zulaica@gmail.com>
2022-08-18 12:15:00 -0700

    Enable background preference toggle

--- source/scripts/preferences.js
+++ source/scripts/preferences.js
@@ -8,6 +8,12 @@ const setInitialTheme = () => {
     : localStorage.setItem('theme', 'light');
 };
 
+const toggleBackground = (event) => {
+  const background = event.target.checked ? 'enabled' : 'disabled';
+  localStorage.setItem('background', background);
+  document.documentElement.setAttribute('data-background', background);
+};
+
 const toggleTheme = (event) => {
   const theme = event.target.checked ? 'dark' : 'light';
   localStorage.setItem('theme', theme);
@@ -19,14 +25,23 @@ const applyUserPreferences = () => {
     setInitialTheme();
   }
 
+  if (!localStorage.getItem('background')) {
+    localStorage.setItem('background', 'enabled');
+  }
+
   const theme = localStorage.getItem('theme');
+  const background = localStorage.getItem('background');
   const menuToggle = document.getElementById('menu-toggle');
   const themeCheckbox = document.getElementById('theme');
+  const backgroundCheckbox = document.getElementById('background');
 
   document.documentElement.setAttribute('data-theme', theme);
+  document.documentElement.setAttribute('data-background', background);
   themeCheckbox.checked = theme === 'dark' ? true : false;
+  backgroundCheckbox.checked = background === 'enabled' ? true : false;
 
   themeCheckbox.addEventListener('change', toggleTheme);
+  backgroundCheckbox.addEventListener('change', toggleBackground);
 
   menuToggle.addEventListener('click', () => {
     isExpanded(menuToggle)
