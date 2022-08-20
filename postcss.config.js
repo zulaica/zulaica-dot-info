26dd4c01bcc600934bbe70f3b36f01426451c35a
David Zulaica <zulaica@gmail.com>
2022-08-19 23:01:37 -0700

    Add build scripts

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,40 @@
 module.exports = {
-  plugins: [
-    require('postcss-import'),
-    require('autoprefixer'),
-    require('cssnano')({ preset: 'default' })
-  ]
+  "plugins": {
+    "postcss-import": {},
+    "postcss-preset-env": {},
+    "postcss-url": [
+      {
+        "filter": "**/*.woff2",
+        "url": "copy"
+      },
+      {
+        "filter": "**/*.woff",
+        "url": "copy"
+      },
+      {
+        "filter": "**/*.ttf",
+        "url": "copy"
+      },
+      {
+        "filter": "**/*.webp",
+        "url": (asset) => asset.relativePath,
+      },
+      {
+        "filter": "**/*.jpg",
+        "url": (asset) => asset.relativePath,
+      },
+    ],
+    "cssnano": {
+      preset: [
+        "advanced",
+        {
+          minifyFontValues: false,
+          normalizeUrl: false,
+          normalizeString: {
+            preferredQuote: 'single'
+          }
+        }
+      ]
+    },
+  },
 };
