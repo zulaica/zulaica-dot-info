126ab88a7f0ca42f6aefeb82325a94731ffe0d8f
David Zulaica <zulaica@gmail.com>
2022-08-27 15:19:31 -0700

    Manage fetching on DOMContentLoaded

--- source/script.hash.js
+++ source/script.hash.js
@@ -9,16 +9,22 @@ import latestSong, {
   stopPolling
 } from './scripts/endpoints/lastfm.js';
 
-const handleLoad = async () => {
-  console.info(`%c ${CONTACT_INFO}`, 'font-family: monospace;');
+const handleDomContentLoaded = async () => {
   await fetchLatestImage();
   await latestSong();
-  applyLatestImage();
-  applyPreferences();
-  renderLatestSong();
   startPolling();
 };
 
-window.addEventListener('load', handleLoad, { passive: true, once: true });
+const handleLoad = () => {
+  console.info(`%c ${CONTACT_INFO}`, 'font-family: monospace;');
+  applyLatestImage();
+  renderLatestSong();
+  applyPreferences();
+};
 
+window.addEventListener('DOMContentLoaded', handleDomContentLoaded, {
+  passive: true,
+  once: true
+});
+window.addEventListener('load', handleLoad, { passive: true, once: true });
 window.addEventListener('unload', stopPolling, { passive: true, once: true });
