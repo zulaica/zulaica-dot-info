83f0158738af7f689906e243263136b9cc86d4bd
David Zulaica <zulaica@gmail.com>
2024-03-09 21:25:56 -0800

    Update VS Code settings and config files

--- source/scripts/helpers/constants.js
+++ source/scripts/helpers/constants.js
@@ -1,18 +1,18 @@
 export const DATETIME_OPTIONS = Object.freeze({
-  day: '2-digit',
-  hour: '2-digit',
+  day: "2-digit",
+  hour: "2-digit",
   hour12: false,
-  minute: '2-digit',
-  month: 'long',
-  timeZone: 'America/Los_Angeles',
-  timeZoneName: 'short',
-  weekday: 'long',
-  year: 'numeric'
+  minute: "2-digit",
+  month: "long",
+  timeZone: "America/Los_Angeles",
+  timeZoneName: "short",
+  weekday: "long",
+  year: "numeric"
 });
 
 export const EMOJI = Object.freeze({
-  partyingFace: '\ud83e\udd73',
-  pensiveFace: '\ud83d\ude14',
-  personShrugging: '\ud83e\udd37',
-  technologist: '\ud83e\uddd1\u200d\ud83d\udcbb'
+  partyingFace: "\ud83e\udd73",
+  pensiveFace: "\ud83d\ude14",
+  personShrugging: "\ud83e\udd37",
+  technologist: "\ud83e\uddd1\u200d\ud83d\udcbb"
 });
