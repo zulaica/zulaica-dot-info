03c69fee509569de1851a9864cdb07346a2a367e
David Zulaica <zulaica@users.noreply.github.com>
2024-11-19 02:12:08 -0800

    v3.0.2 (#65)
    v3.0.2
    
    * Increase toggle size, remove border, adjust background
    
    * Upgrade site-bundler
    
    * Add missing `chalk` dependency
    
    * Update logged contact info
    
    * Fix LastFM
     - Use cached data if available on init
     - Append LastFM elements only if we have data
     - Adjust track details line height
    
    * Update favicon
    
    * Remove Twitter meta tags, update Open Graph image
    
    * Delete Twitter link

--- source/scripts/contact.js
+++ source/scripts/contact.js
@@ -1,11 +1,9 @@
 const CONTACT_INFO = `
            ┌───╴GITHUB╶────┐
-             ┌──────╴WEBSITE╶────────┐
 
   d a v i d @ z u l a i c a . i n f o
 
  └──────────────╴EMAIL╶──────────────┘
-           └───╴SOCIAL╶────┘
 
 `;
 
