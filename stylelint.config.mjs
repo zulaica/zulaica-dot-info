86e73fc9bda13b53d4bc082bccc2c6765de12b3b
David Zulaica <zulaica@users.noreply.github.com>
2025-12-23 17:23:22 -0800

    v3.2.1 (#74)
    * Manage workflow NodeJS installation with volta
    
    * Update dev dependencies and NodeJS version
    
    * Update formatters to ESM
    
    * Update site-bundler

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,9 +1,12 @@
-{
-  "extends": ["stylelint-config-standard"],
-  "rules": {
+/**
+ * @type {import('stylelint').Config}
+ */
+export default {
+  extends: ["stylelint-config-standard"],
+  rules: {
     "block-no-empty": true,
     "font-family-name-quotes": "always-unless-keyword",
     "number-max-precision": 5,
     "shorthand-property-no-redundant-values": null
   }
-}
+};
